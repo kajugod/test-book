8bcb5b7fd2e075af439f0196286314f82c56acb9
kajugod <vuongnq2312@gmail.com>
2024-03-29 23:21:05 +0700

    test

---
+++ misskon/src/genre.js
@@ -0,0 +1,5 @@
+function execute() {
+    return Response.success([
+        {title: "buon dua", input: "https://misskon.com/top3/", script: "gen.js"},
+    ]);
+}
---
+++ misskon/src/home.js
@@ -0,0 +1,5 @@
+function execute() {
+    return Response.success([
+        {title: "Top 3", input: "https://misskon.com/top3/", script: "gen.js"}
+    ]);
+}
