752eb9121be1d0ceff5e5403200343859f014047
kajugod <vuongnq2312@gmail.com>
2024-03-31 22:10:57 +0700

    aaaa

--- hentai-img/src/home.js
+++ hentai-img/src/home.js
@@ -1,6 +1,12 @@
 function execute() {
     return Response.success([
-        {title: "Ranking", input: "https://hentai-img.com/ranking/", script: "gen.js"}
-
+        {title: "Home", input: "https://hentai-img.com/", script: "gen.js"},
+        {title: "Ranking", input: "https://hentai-img.com/ranking/", script: "gen.js"},
+        {title: "Ranking Image", input: "https://hentai-img.com/ranking-images/", script: "gen.js"},
+        {title: "AI Generated", input: "https://hentai-img.com/search/keyword/AI+Generated/", script: "gen.js"},
+        {title: "Tag", input: "https://hentai-img.com/tag/", script: "gen.js"},
+        {title: "Pixiv", input: "https://hentai-img.com/search/tag/pixiv/", script: "gen.js"},      
+        {title: "Pixiv Search", input: "https://hentai-img.com/search/keyword/pixiv/page/1/", script: "gen.js"},
+        {title: "离为火火", input: "https://hentai-img.com/search/keyword/离为火火/page/1/", script: "gen.js"}
     ]);
 }
